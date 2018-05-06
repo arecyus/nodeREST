85cec94375be753df2a62395a7d601ab4b7657f6
Arecyus <emailfacu@gmail.com>
2018-05-06 13:25:29 -0300

    Rest server

--- server/server.js
+++ server/server.js
@@ -1,43 +1,23 @@
 require("./config/config");
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const bodyParser = require('body-parser');
 
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use(require('./routes/usuario'));
 
 
 app.get("/", (req, res) => {
     res.json("h");
 })
 
-app.get("/usuario", (req, res) => {
-    res.send("Probando")
-})
-app.post("/usuario", (req, res) => {
-    let body = req.body;
-    if (body.nombre === undefined) {
-        res.status(400).send({
-            ok: false,
-            mensaje: "El nombre es necesario"
-        });
-    } else {
-        res.send({ body });
-    }
-
-})
-
-app.put("/usuario/:id", (req, res) => {
-    let id = req.params.id;
-    res.send({
-        id
-    });
+mongoose.connect(process.env.URLDB, (err, res) => {
+    if (err) throw err;
+    console.log("Base de datos online");
 });
-
-app.delete("/usuario", (req, res) => {
-    res.send("Borrar")
-})
 
 app.listen(port, () => {
     console.log(`${port}`);
